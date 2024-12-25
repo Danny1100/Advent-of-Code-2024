b596bdd92ea570595e1093136684d4156af31058
Danny Cai <dannycai1100@gmail.com>
2024-12-25 15:03:44 +1100

    solve 16

--- Advent of Code/Day 16/Solution.py
+++ Advent of Code/Day 16/Solution.py
@@ -1,12 +1,12 @@
-import sys
-sys.setrecursionlimit(100000)
+import heapq
+import copy
 
 grid = []
 directions = {
-    'N': (-1, 0),    # North: move up (decrease y)
-    'S': (1, 0),     # South: move down (increase y)
-    'E': (0, 1),     # East: move right (increase x)
-    'W': (0, -1),    # West: move left (decrease x)
+    "N": (-1, 0),  # North: move up (decrease y)
+    "S": (1, 0),  # South: move down (increase y)
+    "E": (0, 1),  # East: move right (increase x)
+    "W": (0, -1),  # West: move left (decrease x)
 }
 with open("Input.txt", "r") as file:
     for line in file:
@@ -14,28 +14,62 @@ with open("Input.txt", "r") as file:
         grid.append(line)
 rows, cols = len(grid), len(grid[0])
 
-# Problem 1
-def dfs(i, j, direction, points):
-    if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == '#' or (i, j) in visited:
-        return float('inf')
-    if grid[i][j] == 'E':
-        return points
-    
-    visited.add((i, j))
-    res = float('inf')
-    for d in directions:
-        new_i, new_j = i + directions[d][0], j + directions[d][1]
-        if d == direction:
-            res = min(dfs(new_i, new_j, d, points+1), res)
-        else:
-            res = min(dfs(new_i, new_j, d, points+1001), res)
-    visited.remove((i, j))
-    return res
+
+def print_grid():
+    for row in grid:
+        print(row)
+
 
 for i in range(rows):
     for j in range(cols):
-        if grid[i][j] == 'S':
-            visited = set()
-            result = dfs(i, j, 'E', 0)
+        if grid[i][j] == "S":
+            sx, sy = i, j
+
+
+# Problem 1
+def djikstras(sx, sy):
+    result = set()
+    shortest = float("inf")
+    visited = set()
+    pq = []
+    heapq.heappush(pq, (0, [(sx, sy)], sx, sy, "E"))
+    while pq:
+        distance, path, x, y, direction = heapq.heappop(pq)
+        visited.add((x, y, direction))
+
+        if distance > shortest:
             break
-print(result)
+
+        if grid[x][y] == "E":
+            shortest = distance
+            # print(path)
+            # print(pq)
+            for p in path:
+                result.add(p)
+
+        for d_name, d_vals in directions.items():
+            nx, ny = x + d_vals[0], y + d_vals[1]
+            if (
+                nx >= 0
+                and nx < rows
+                and ny >= 0
+                and ny < cols
+                and (nx, ny, d_name) not in visited
+                and grid[nx][ny] != "#"
+            ):
+                new_path = copy.deepcopy(path)
+                new_path.append((nx, ny))
+                if d_name == direction:
+                    heapq.heappush(pq, (distance + 1, new_path, nx, ny, d_name))
+                else:
+                    heapq.heappush(pq, (distance + 1001, new_path, nx, ny, d_name))
+    return result
+
+
+result = djikstras(sx, sy)
+# print(result)
+print(len(result))
+# for x, y in result:
+#     grid[x][y] = "O"
+
+# print_grid()
