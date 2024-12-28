0b77e61a212cd774765fb9fedd86e534b1ec2137
Danny Cai <dannycai1100@gmail.com>
2024-12-28 21:49:10 +1100

    24p2

--- Advent of Code/Day 24/24.py
+++ Advent of Code/Day 24/24.py
@@ -45,3 +45,40 @@ for _, n in ls:
     res.insert(0, str(n))
 result = int("".join(res), 2)
 print(result)
+
+# Problem 2
+x = []
+for key, val in cache.items():
+    if key.startswith("x"):
+        x.append((key, val))
+x.sort()
+bin1 = []
+for _, n in x:
+    bin1.insert(0, str(n))
+bin1 = "".join(bin1)
+
+y = []
+for key, val in cache.items():
+    if key.startswith("y"):
+        y.append((key, val))
+y.sort()
+bin2 = []
+for _, n in y:
+    bin2.insert(0, str(n))
+bin2 = "".join(bin2)
+
+int_sum = int(bin1, 2) + int(bin2, 2)
+# Convert the sum back to binary and remove the '0b' prefix
+expected = bin(int_sum)[2:]
+
+print("expected: ", expected)
+print(
+    "actual: ",
+    "".join(res),
+)
+int_diff = int("".join(res), 2) - int(expected, 2)
+diff = bin(int_diff)[2:]
+print("diff: ", diff)
+
+ans = ["z14", "hbk", "z18", "kvn", "z23", "dbb", "tfn", "cvh"]
+print(",".join(sorted(ans)))
